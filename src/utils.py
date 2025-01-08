232af2fb4e455fd6d6c3df9e9bf27019add18c82
melatteshome <melatteshome9@gmail.com>
2025-01-08 23:03:48 +0300

    check missing alue function

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,28 @@ def check_duplicates(df):
         return df[duplicates]
     else:
         return 'no duplicates found'
+    
+
+def check_missing_values(df):
+    missing_values = df.isnull().sum()
+
+    missing_values_datatype =  df.dtypes[missing_values > 0]
+
+    missing_values_percentage = missing_values[missing_values > 0] / df.shape[0] * 100
+
+    missing_values_info = pd.DataFrame({
+        'missing_values': missing_values[missing_values > 0],
+        'missing_values_percentage': missing_values_percentage,
+        'datatype': missing_values_datatype
+    })
+
+    return missing_values_info
+
+
+
+    
+# def check_for_outliers()
+
 
 
 
