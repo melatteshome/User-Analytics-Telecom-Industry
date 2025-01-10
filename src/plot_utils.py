a2b761dfe30dc252be7d79193669aaad14b9eb7a
melatteshome <melatteshome9@gmail.com>
2025-01-10 23:17:20 +0300

    plot _boxplot function added

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -21,3 +21,28 @@ def plot_hist(df, col, title, xlabel, ylabel, bins=10, figsize=(10, 6)):
     plt.ylabel(ylabel)
     plt.show()
 
+def plot_boxplot(data, title, x_label, color='lightcoral'):
+    """
+    Plot a boxplot.
+
+    Parameters:
+    - data: Series or array-like
+        Data to be plotted.
+    - title: str
+        Plot title.
+    - x_label: str
+        Label for the x-axis.
+    - color: str, optional
+        Color for the plot.
+
+    Returns:
+    - None
+    """
+    plt.figure(figsize=(8, 6))
+    sns.boxplot(x=data, color=color)
+    plt.title(title, fontsize=16)
+    plt.xlabel(x_label, fontsize=14)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.show()
